6dd2b5bf6d576bd739dc7d1dd18df292388d4b55
Hussain <ask2md@gmail.com>
2017-10-27 01:52:35 +0530

    fixing fb id and secret

--- facebook/config/auth.js
+++ facebook/config/auth.js
@@ -3,8 +3,8 @@
  */
 module.exports = {
     'facebookAuth' : {
-        'clientID'      :   '271200740023977',
-        'clientSecret'  :   '176225e14616ece13955fc92c24f9c95',
+        'clientID'      :   process.env.FACEBOOK_APP_ID,
+        'clientSecret'  :   process.env.FACEBOOK_APP_SECRET,
         'callbackURL'   :   'http://localhost:3000/auth/facebook/callback'
     }
 }
