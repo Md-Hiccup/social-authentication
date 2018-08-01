f407c30155934d77311f244f8d85aa388760fa9a
Md-Hiccup <ask2md@gmail.com>
2018-08-01 17:40:41 +0530

    Writing Readme

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function(req, res){
     res.writeHead(200, {'content-type':'text/plain'});
     res.end("hello World!!!!");
-}).listen('1337');
-console.log("Server is running on 8080");
+}).listen('3000');
+console.log("Server is running on 3000");
